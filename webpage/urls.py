99a1f1a105c25a65bf31b85781f419a15de8a0e5
Nonny <talessonli489@gmail.com>
2025-09-03 13:56:48 +0700

    first webpage

---
+++ webpage/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+
+    path('base/', views.base, name='base'),
+
+    path('login/', views.login, name='login'),
+]
